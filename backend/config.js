2c4151698e665c01ffdb73c1b8ee89312123ca26
unknown <75951157+mohammed430@users.noreply.github.com>
2021-05-06 21:32:47 -0600

    first commit

--- backend/config.js
+++ backend/config.js
@@ -1,19 +1,3 @@
-// var mysql = require('mysql');
-
-
-
-// var con = mysql.createConnection({
-//   host: "127.0.0.1",
-//   user: "Moe",
-//   password: "Hello123123",
-//   database: "booker"
-// });
-
-// con.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-// });
-
 const SequelizeClass = require('sequelize');
 const sequelize = new SequelizeClass('booker', 'Moe','Hello123123', {dialect: 'mysql'});
 
